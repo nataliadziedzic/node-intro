ce9491cb8d95352d424010f5dc4b7d908735ee74
Natalia Dziedzic <nd.nataliadziedzic@gmail.com>
2021-02-07 15:15:04 +0100

    Update for heroku

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const functions = require('./functions')
 
-const port = 3000
+const port = process.env.PORT || 3000
 const app = express()
 app.set('view engine', 'hbs')
 app.use('/assets', express.static(path.join(__dirname, './assets')))
